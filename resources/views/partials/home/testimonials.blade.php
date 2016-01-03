706212a6d0b1ae5cac8fe3fac62261886118a940
vagrant <vagrant@homestead>
2016-01-03 14:23:49 +0000

    Added portfolio's: W. de Roode, O. Veldman, Mert Ozturk; and added edited J.L. Sparreboom: Added summary of work done.

--- resources/views/partials/home/testimonials.blade.php
+++ resources/views/partials/home/testimonials.blade.php
@@ -7,16 +7,44 @@
                     <!-- Wrapper for slides -->
                     <div class="carousel-inner" role="listbox">
                         <div class="item active">
-                            <p><img class="img-circle img-thumbnail" src={{ URL::asset("img/testimonial/01.jpg") }} alt=""></p>
+                            <p><img class="img-circle img-thumbnail"
+                                    src={{ URL::asset("img/testimonial/01.jpg") }} alt="Mugshot_Sjors-Sparreboom"></p>
                             <h4>J.L. Sparreboom</h4>
                             <small>Front end designer, maintainer and part of the clean up crew</small>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut et dolore magna aliqua. Ut enim ad minim veniam</p>
+                            <p>Full front end designer and programmer also contributed designing the database and
+                                cleaning up back end code including documentation.</p>
                         </div>
                         <div class="item">
-                            <p><img class="img-circle img-thumbnail" src={{ URL::asset("img/testimonial/01.jpg") }} alt=""></p>
-                            <h4>J.L. Sparreboom</h4>
-                            <small>Front end designer, maintainer and part of the clean up crew</small>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut et dolore magna aliqua. Ut enim ad minim veniam</p>
+                            <p><img class="img-circle img-thumbnail"
+                                    src={{ URL::asset("img/testimonial/03.jpg") }} width="88" height="88"
+                                    alt="Mugshot_Oscar-Veldman"></p>
+                            <h4>O. Veldman</h4>
+                            <small>Back end designer, maintainer and part of the IT Magician's movement</small>
+                            <p>Full back end designer and programmer also contributed integrating the database
+                                connection and fixing queries.</p>
+                        </div>
+                        <div class="item">
+                            <p><img class="img-circle img-thumbnail"
+                                    src={{ URL::asset("img/testimonial/02.png") }} width="88" height="88"
+                                    alt="Mugshot_Wesley-de-Roode"></p>
+                            <h4>W. de Roode</h4>
+                            <small>Back end, Front end and Database designer, helping hand and part of the clean up
+                                crew
+                            </small>
+                            <p>Designed and built the database including the connection to the back end with help from
+                                O. Veldman and J.L. Sparreboom. Also contributed in programming the front end of the
+                                project and refurbishing bad documentation.</p>
+                        </div>
+                        <div class="item">
+                            <p><img class="img-circle img-thumbnail"
+                                    src={{ URL::asset("img/testimonial/04.jpg") }} width="88" height="88"
+                                    alt="Mugshot_Mert-Özturk"></p>
+                            <h4>Mert Özturk</h4>
+                            <small>Back end helper and programmer, also part of the clean up crew
+                            </small>
+                            <p>Researched different techniques and helped with programming the back end of the project.
+                                Also build the PDF creator function to generate reports and contributed to improving the
+                                communication.</p>
                         </div>
                     </div>
 
