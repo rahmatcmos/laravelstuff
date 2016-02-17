8995455329f3b932a1bf1e5ba21bd405e1f63ab9
RobinBakker <robinb@live.nl>
2016-02-17 01:41:05 +0100

    updated unittests

---
+++ tests/UserTest.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\User;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class UserTest extends TestCase
+{
+
+    /** @test */
+    public function testHome()
+    {
+        $this->visit('/')
+             ->see('CityGis');
+    }
+
+
+    /** @test */
+    public function testError()
+    {
+        $respons = $this->call('GET', '/about/');
+        $this->assertEquals(404, $response->status());
+    }
+
+    /** @test */
+    public function testHash()
+    {
+        $this->visit('/')
+             ->click('LEES MEER')
+             ->seePageIs('/');
+    }
+
+    /**
+     * @param null $user
+     * @return $this
+     */
+    protected function login($user = null)
+    {
+        $user = $user ?: factory(App\User::class)->create([ 'Password' => 'secret' ]);
+
+        return $this->visit('login')->type($user->email, 'email')->type('secret', 'password')->press('Login');
+    }
+}
